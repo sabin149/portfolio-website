312ff1eb64e135607ea0b8dcbfc361110532520d
sabin149 <dangalsabin149@gmail.com>
2023-03-19 19:29:02 +0545

    code updated

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,65 +1,57 @@
 const Footer = () => {
   return (
-    <footer className="text-gray-600 h-32 lg:h-20 bg-[#e6e6e6] flex flex-col items-center justify-center py-5 mt-8">
+    <footer className="mt-8 flex h-32 flex-col items-center justify-center bg-[#e6e6e6] py-5 text-gray-600 lg:h-20">
       <div className="flex flex-col gap-[0.015rem]">
-        <div className="flex flex-row justify-center items-center mt-1">
-          <a className="text-gray-500 mr-2">
+        <div className="mt-1 flex flex-row items-center justify-center">
+          <a
+            href="https://facebook.com"
+            className="mr-2 text-gray-500 hover:text-blue-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-8 h-8"
+              className="h-8 w-8"
               viewBox="0 0 24 24"
             >
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a className=" text-gray-500 mr-2">
+          <a
+            href="https://twitter.com"
+            className=" mr-2 text-gray-500 hover:text-sky-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-8 h-8"
+              className="h-8 w-8"
               viewBox="0 0 24 24"
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className=" text-gray-500 mr-2">
+          <a
+            href="https://instagram.com"
+            className=" mr-2 text-gray-500 hover:text-red-400"
+          >
             <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-8 h-8"
+              className="h-8 w-8"
               viewBox="0 0 24 24"
             >
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a className=" text-gray-500 mr-2">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="0"
-              className="w-8 h-8"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
         </div>
-        <div className="flex flex-col lg:flex-row justify-center items-center md:text-lg text-base flex-wrap">
+        <div className="flex flex-col flex-wrap items-center justify-center text-base md:text-lg lg:flex-row">
           <p>&copy; 2023. All Right Reserved. </p>
           <p className="mr-1">kritikrawal.com.np | </p>
           <p>Designed & Developed By Kritik Rawal</p>
--- src/components/Home/Expertise.tsx
+++ src/components/Home/Expertise.tsx
@@ -9,7 +9,7 @@ const Expertise = () => {
             <h1 className="title-font text-lg font-semibold uppercase text-gray-900 sm:text-xl md:text-2xl lg:text-3xl">
               Expertise
             </h1>
-            <hr className="mt-1 mb-2 block h-1.5 w-[82%] rounded bg-indigo-500 sm:w-[26.5rem] " />
+            <hr className="mb-4 block h-1.5 w-[82%] rounded bg-indigo-500 sm:w-[26.5rem] " />
           </div>
         </div>
         <div>
@@ -18,10 +18,10 @@ const Expertise = () => {
               {expertiseData.map((expertise) => {
                 return (
                   <div
-                    className="w-full p-3 sm:w-1/2 sm:px-5 lg:w-1/3"
+                    className="w-full px-3 py-1 sm:w-1/2 sm:px-5 lg:w-1/3"
                     key={expertise.id}
                   >
-                    <div className="max-h-full overflow-hidden rounded-sm border-opacity-60 bg-[#EC733F] px-4 pt-4 shadow-md">
+                    <div className="max-h-full overflow-hidden rounded-sm border-opacity-60 bg-[#EC733F] px-3 pt-4 shadow-md">
                       <img
                         className="h-60 w-full object-cover object-center transition duration-500 ease-in-out hover:scale-[1.02]"
                         src={expertise.imageUrl}
