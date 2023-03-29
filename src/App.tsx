791ec39e8b0723472c0abf26bc8e54d07bbc9944
sabin149 <dangalsabin149@gmail.com>
2023-03-29 21:56:33 +0545

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,24 @@ import ErrorFallback from "components/Common/ErrorFallback";
 
 function App() {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Suspense
-        fallback={
-          <div className="fixed top-0 left-0 flex h-full w-full flex-col items-center justify-center bg-white bg-opacity-50">
-            <div className="h-32 w-32 animate-spin rounded-full border-t-2 border-b-2 border-indigo-500"></div>
-            <h2
-              className="mt-3 ml-4 block animate-pulse text-2xl
-            font-semibold tracking-wide
-            text-gray-700
-            "
-            >
-              Loading...
-            </h2>
-          </div>
-        }
-      >
-        <RouterProvider router={Router} />
-      </Suspense>
-    </ErrorBoundary>
+    // <ErrorBoundary FallbackComponent={ErrorFallback}>
+    //   <Suspense
+    //     fallback={
+    //       <div className="fixed top-0 left-0 flex h-full w-full flex-col items-center justify-center bg-white bg-opacity-50">
+    //         <div className="h-32 w-32 animate-spin rounded-full border-t-2 border-b-2 border-indigo-500"></div>
+    //         <h2
+    //           className="mt-3 ml-4 block animate-pulse text-2xl
+    //         font-semibold tracking-wide
+    //         text-gray-700
+    //         "
+    //         >
+    //           Loading...
+    //         </h2>
+    //       </div>
+    //     }
+    //   >     </Suspense>
+    // </ErrorBoundary>
+    <RouterProvider router={Router} />
   );
 }
 export default App;
