034279d2798f3bea8246fc02370f6c0081c81619
sabin149 <dangalsabin149@gmail.com>
2023-03-30 00:00:09 +0545

    contact page updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ import ContactForm from "components/Contact/ContactForm";
 
 const Contact = () => {
   return (
-    <section className="flex flex-shrink-0 flex-wrap items-center justify-center px-2 lg:px-0 3xl:min-h-screen">
+    <section className="flex flex-shrink-0 flex-wrap items-center justify-center px-2 lg:px-0">
       <div className="mx-auto mt-6 max-w-3xl overflow-hidden rounded-lg border px-5 py-5 shadow-md">
         <div className="mb-5 px-4">
           <h2 className="border-b-2 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
